8af01b9f2ff53072a6e75267bda36a65b7fdd0f5
Jumoke Oladimeji <jumoke5ng@yahoo.com>
2018-08-03 10:00:08 +0100

    Use the api url for getting the lyrics

---
+++ api/lib/data.js
@@ -0,0 +1,72 @@
+module.exports = [
+  {
+    "title": "LSD (feat. Chance The Rapper)",
+    "artist": "Jamila Woods",
+    "album": "HEAVN",
+    "albumArtURI": "http://192.168.0.10:1400/getaa?s=1&u=x-sonos-spotify%3aspotify%253atrack%253a1cBs15tQeh4ewFqlwndESK%3fsid%3d12%26flags%3d8224%26sn%3d1",
+    "uri": "x-sonos-spotify:spotify%3atrack%3a1cBs15tQeh4ewFqlwndESK?sid=12&flags=8224&sn=1"
+  },
+  {
+    "title": "I Might Need Security",
+    "artist": "Chance the Rapper",
+    "album": "I Might Need Security",
+    "albumArtURI": "http://192.168.0.10:1400/getaa?s=1&u=x-sonos-spotify%3aspotify%253atrack%253a3EApebexZ7YqDIqw2EMTDh%3fsid%3d12%26flags%3d8224%26sn%3d1",
+    "uri": "x-sonos-spotify:spotify%3atrack%3a3EApebexZ7YqDIqw2EMTDh?sid=12&flags=8224&sn=1"
+  },
+  {
+    "title": "Wala Cam",
+    "artist": "Chance the Rapper",
+    "album": "Wala Cam",
+    "albumArtURI": "http://192.168.0.10:1400/getaa?s=1&u=x-sonos-spotify%3aspotify%253atrack%253a2cEnYJKLSZPH7MFSk6C05c%3fsid%3d12%26flags%3d8224%26sn%3d1",
+    "uri": "x-sonos-spotify:spotify%3atrack%3a2cEnYJKLSZPH7MFSk6C05c?sid=12&flags=8224&sn=1"
+  },
+  {
+    "title": "Work Out",
+    "artist": "Chance the Rapper",
+    "album": "Work Out",
+    "albumArtURI": "http://192.168.0.10:1400/getaa?s=1&u=x-sonos-spotify%3aspotify%253atrack%253a3PdgIdRXJjgxfOr7slldel%3fsid%3d12%26flags%3d8224%26sn%3d1",
+    "uri": "x-sonos-spotify:spotify%3atrack%3a3PdgIdRXJjgxfOr7slldel?sid=12&flags=8224&sn=1"
+  },
+  {
+    "title": "65th & Ingleside",
+    "artist": "Chance the Rapper",
+    "album": "65th & Ingleside",
+    "albumArtURI": "http://192.168.0.10:1400/getaa?s=1&u=x-sonos-spotify%3aspotify%253atrack%253a5H0NU2sHWUnWCcZyYlX46X%3fsid%3d12%26flags%3d8224%26sn%3d1",
+    "uri": "x-sonos-spotify:spotify%3atrack%3a5H0NU2sHWUnWCcZyYlX46X?sid=12&flags=8224&sn=1"
+  },
+  {
+    "title": "No Problem (feat. Lil Wayne & 2 Chainz)",
+    "artist": "Chance the Rapper",
+    "album": "Coloring Book",
+    "albumArtURI": "http://192.168.0.10:1400/getaa?s=1&u=x-sonos-spotify%3aspotify%253atrack%253a0v9Wz8o0BT8DU38R4ddjeH%3fsid%3d12%26flags%3d8224%26sn%3d1",
+    "uri": "x-sonos-spotify:spotify%3atrack%3a0v9Wz8o0BT8DU38R4ddjeH?sid=12&flags=8224&sn=1"
+  },
+  {
+    "title": "Best Life (feat. Chance The Rapper)",
+    "artist": "Cardi B",
+    "album": "Invasion of Privacy",
+    "albumArtURI": "http://192.168.0.10:1400/getaa?s=1&u=x-sonos-spotify%3aspotify%253atrack%253a00B7TZ0Xawar6NZ00JFomN%3fsid%3d12%26flags%3d8224%26sn%3d1",
+    "uri": "x-sonos-spotify:spotify%3atrack%3a00B7TZ0Xawar6NZ00JFomN?sid=12&flags=8224&sn=1"
+  },
+  {
+    "title": "Juke Jam (feat. Justin Bieber & Towkio)",
+    "artist": "Chance the Rapper",
+    "album": "Coloring Book",
+    "albumArtURI": "http://192.168.0.10:1400/getaa?s=1&u=x-sonos-spotify%3aspotify%253atrack%253a3eze1OsZ1rqeXkKStNfTmi%3fsid%3d12%26flags%3d8224%26sn%3d1",
+    "uri": "x-sonos-spotify:spotify%3atrack%3a3eze1OsZ1rqeXkKStNfTmi?sid=12&flags=8224&sn=1"
+  },
+  {
+    "title": "All Night (feat. Knox Fortune)",
+    "artist": "Chance the Rapper",
+    "album": "Coloring Book",
+    "albumArtURI": "http://192.168.0.10:1400/getaa?s=1&u=x-sonos-spotify%3aspotify%253atrack%253a60xaS8mYBKUW4VQQ666N0T%3fsid%3d12%26flags%3d8224%26sn%3d1",
+    "uri": "x-sonos-spotify:spotify%3atrack%3a60xaS8mYBKUW4VQQ666N0T?sid=12&flags=8224&sn=1"
+  },
+  {
+    "title": "Same Drugs",
+    "artist": "Chance the Rapper",
+    "album": "Coloring Book",
+    "albumArtURI": "http://192.168.0.10:1400/getaa?s=1&u=x-sonos-spotify%3aspotify%253atrack%253a6m9qPYXmhge2QhBLfFKnVF%3fsid%3d12%26flags%3d8224%26sn%3d1",
+    "uri": "x-sonos-spotify:spotify%3atrack%3a6m9qPYXmhge2QhBLfFKnVF?sid=12&flags=8224&sn=1"
+  }
+]
--- api/lib/musicxmatch.js
+++ api/lib/musicxmatch.js
@@ -4,34 +4,65 @@ const MXMUtil = MusixmatchSDK.Util;
 const MXMLogger = MusixmatchSDK.Logger;
 const MXMAPI = MusixmatchSDK.API;
 MXMLogger.setLevel(MXMLogger.LEVEL_INFO);
+const request = require('request')
+const axios = require('axios')
 
+const testData = require('./data')
 class GetSongLyrics {
   constructor() {
-    MXMLogger.setLevel(MXMLogger.LEVEL_INFO);
-    MXMAPI.init({ 
-      apiKey: process.env.MUSIXMATCH_API_KEY,
-      logger : MXMLogger, 
-      debug : false, 
-      persistentCache : true, 
-      memCache : false, 
-      cacheFolderPath : './cache/' 
-    }).then((result) => {
-      MXMLogger.info('done initializing')
-    })
-    .catch(error => {
-      MXMLogger.error("API error %@", error);
-    });
+    this.queue = testData // to be changed to data coming from sonos
   }
-
+  async getwithaxios() {
+    const songLyricsArray = [];
+    songLyricsArray.push(await this.queue.map((songObj) => {
+      axios.get('https://api.musixmatch.com/ws/1.1/matcher.lyrics.get', {
+        params: {
+          q_track: songObj.title, 
+          q_artist: songObj.artist, 
+          apikey: process.env.MUSIXMATCH_API_KEY
+        }
+      })
+      .then(response => {
+        const body = response.data;
+        const lyrics = body.message.body.lyrics.lyrics_body
+        const obj = { ...songObj, lyrics}
+        return obj;
+        // songLyricsArray.push(obj);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }))
+    console.log('songLyricsArray', songLyricsArray)  
+  }
   async getSongIdAndLyrics() {
     const songLyricsArray = [];
-    const songLyrics = await this.queue.map((songObj) => {
+    let songLyrics = {}
+    for (let i = 0; i < this.queue.length; i += 1) {
+      const songObj = this.queue[i];
       const input = {q_track: songObj.title, q_artist: songObj.artist, apikey: process.env.MUSIXMATCH_API_KEY};
-      const trackId = this.getTrackId(input);
-      return this.getSongLyrics(trackId);  
-    })
-    songLyricsArray.push(songLyricsArray);
-    return
+      await request.get({
+        url: 'https://api.musixmatch.com/ws/1.1/matcher.lyrics.get', 
+        qs: input, 
+        jsonP: true},
+        (error, response) => {
+        const body = JSON.parse(response.body)
+        const lyrics = body.message.body.lyrics.lyrics_body
+        const obj = { ...songObj, lyrics}
+        songLyricsArray.push(obj);
+      });
+    }
+    // return songLyricsArray 
+  }
+
+  async getMatcherTrack(input) {
+    request.get({
+      url: 'https://api.musixmatch.com/ws/1.1/matcher.lyrics.get', 
+      qs:input, 
+      jsonP:true},
+      (error, response) => {
+      return response.body.lyrics_body;
+    });
   }
 
   getTrackId(songObj) {
